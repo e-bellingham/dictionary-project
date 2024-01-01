5697e881ebd22d3bf8c5ba5ac43a48247943494a
Elizabeth <e.bellingham02@gmail.com>
2023-12-31 22:22:15 -0500

    added photo element to dictionary search

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,10 +2,12 @@ import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(" ");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -15,13 +17,19 @@ export default function Dictionary() {
     event.preventDefault();
     const apiKey = "26abe423762oeff8b3623ddt062bace3";
     const apiURL = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiURL).then(handleResponse);
+    axios.get(apiURL).then(handleDictionaryResponse);
+
+    const apiPhotoKey = "26abe423762oeff8b3623ddt062bace3";
+    const apiPhotoUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiPhotoKey}`;
+    axios.get(apiPhotoUrl).then(handlePhotoResponse);
   }
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data);
   }
-
+  function handlePhotoResponse(response) {
+    setPhotos(response.data.photos);
+  }
   return (
     <div className="Dictionay">
       <section>
@@ -37,6 +45,7 @@ export default function Dictionary() {
         <small>suggested words: sunset, wine, yoga...</small>
       </section>
       {results && <Results results={results} />}
+      <Photos photos={photos} />
     </div>
   );
 }
---
+++ src/Photos.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Photo.css";
+
+export default function Photos(props) {
+  console.log(props.photos);
+  if (props.photos) {
+    return (
+      <section className="Photos">
+        {props.photos.map(function (photo, index) {
+          return (
+            <img
+              className="img-fluid"
+              key={index}
+              src={photo.src.landscape}
+              alt={photo.alt}
+            />
+          );
+        })}
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
